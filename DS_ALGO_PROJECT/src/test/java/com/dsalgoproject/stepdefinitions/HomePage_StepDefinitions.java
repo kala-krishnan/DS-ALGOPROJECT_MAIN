26c5abcd8dbfdd9aecc1f0d932baa24f87f4e794
kala-krishnan <krishna@10.0.0.26>
2023-07-28 23:55:17 -0400

    Completed HomePage scenarios

--- DS_ALGO_PROJECT/src/test/java/com/dsalgoproject/stepdefinitions/HomePage_StepDefinitions.java
+++ DS_ALGO_PROJECT/src/test/java/com/dsalgoproject/stepdefinitions/HomePage_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.dsalgoproject.stepdefinitions;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 import com.dsalgoproject.apphooks.Hooks;
 import com.dsalgoproject.pageobjects.HomePage_PageObject;
@@ -30,8 +31,9 @@ public class HomePage_StepDefinitions {
 
 	@Then("The User should be redirected to homeage")
 	public void the_user_should_be_redirected_to_homeage() {
-		
-		System.out.println("The User should be redirected to HomePage");
+		String actualPageTitle = homePageObj.getErrormessage();
+		String ExpectedPageTitle = driver.getTitle();
+		Assert.assertEquals(actualPageTitle, ExpectedPageTitle);
 	}
 	@Given("The User is in HomePage")
 	public void the_user_is_in_home_page() {
@@ -45,21 +47,43 @@ public class HomePage_StepDefinitions {
 
 	@Then("the user will get the {string}")
 	public void the_user_will_get_the(String string) {
-		System.out.println("The error message " +string);
+		String actualErrorMessage = homePageObj.getErrormessage();
+		Assert.assertEquals(actualErrorMessage, string);
 	}
 
-//	@When("The user clicks on Get Started links on homepage without login")
-//	public void the_user_clicks_on_get_started_links_on_homepage_without_login() {
-//		
-//	
-//	}
-//
-//	@Then("the error message {string} will display")
-//	public void the_error_message_will_display(String string) {
-//		
-//
-//	}
+	@When("The user clicks GetStarted button in {string} without login")
+	public void the_user_clicks_get_started_button_in_without_login(String string) throws InterruptedException {
+		homePageObj.dataStrGetStartedButtonClick(string);
+	}
 
+	@Then("The user will get the following {string}")
+	public void the_user_will_get_the_following(String string) {
+		String actualErrorMessage = homePageObj.getErrormessage();
+		Assert.assertEquals(actualErrorMessage, string);
+	}
 
+	@When("The user clicks sign in")
+	public void the_user_clicks_sign_in() {
+		homePageObj.goToSignPage();
+	}
+
+	@Then("The user should be redirected to sign in page")
+	public void the_user_should_be_redirected_to_sign_in_page() {
+		String actualPageTitle = homePageObj.getErrormessage();
+		String ExpectedPageTitle = driver.getTitle();
+		Assert.assertEquals(actualPageTitle, ExpectedPageTitle);
+	}
+
+	@When("The User clicks Register")
+	public void the_user_clicks_register() {
+		homePageObj.goToRegisterPage();
+	}
+
+	@Then("The user should be redirected to register page")
+	public void the_user_should_be_redirected_to_register_page() {
+		String actualPageTitle = homePageObj.getErrormessage();
+		String ExpectedPageTitle = driver.getTitle();
+		Assert.assertEquals(actualPageTitle, ExpectedPageTitle);
+	}
 
 }
--- DS_ALGO_PROJECT/src/test/resources/feature/00_HomePageFeature.feature
+++ DS_ALGO_PROJECT/src/test/resources/feature/00_HomePageFeature.feature
@@ -25,6 +25,8 @@ Feature: To Test the HomePage
     When The User clicks the "Get Started" button
     Then The User should be redirected to homeage
     
+    
+    
   Scenario Outline: User is clicking Data Structures
   
   Given  The User is in HomePage
@@ -40,5 +42,33 @@ Feature: To Test the HomePage
  | Tree    | You are not logged in      |
  | Graph    | You are not logged in     |   
 
+Scenario Outline: User is on HomePage and click getstarted link on homepage without logged in
+Given The User is in HomePage
+When The user clicks GetStarted button in "<options>" without login
+Then The user will get the following "<errormessage>"
+
+Examples:
+ | options        | errormessage  |
+ | Data Structures-Introduction      | You are not logged in  |
+ | Arrays      | You are not logged in  |
+ | Linked List | You are not logged in  |
+ | Stack    | You are not logged in     |
+ | Queue    | You are not logged in     |
+ | Tree    | You are not logged in      |
+ | Graph    | You are not logged in     |   
+ 
+ Scenario: User is on Home page and click on sign in
+ Given The User is in HomePage
+ When The user clicks sign in
+ Then The user should be redirected to sign in page
+ 
+ Scenario: User is on Home page and click register
+ Given The User is in HomePage
+ When The User clicks Register
+ Then The user should be redirected to register page
+ 
+ 
+ 
+ 
 
  
