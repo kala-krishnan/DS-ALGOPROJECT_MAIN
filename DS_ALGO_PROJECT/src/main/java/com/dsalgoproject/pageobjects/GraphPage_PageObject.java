6599c3599f393df29aa864d2e3433bbe80a7988a
kala-krishnan <krishna@10.0.0.26>
2023-07-31 08:42:26 -0400

    Completed Graph option Menus and committing the same

---
+++ DS_ALGO_PROJECT/src/main/java/com/dsalgoproject/pageobjects/GraphPage_PageObject.java
@@ -0,0 +1,123 @@
+package com.dsalgoproject.pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.dsalgoproject.utility.CommonUtils;
+
+
+
+public class GraphPage_PageObject {
+	
+	WebDriver driver;
+	Actions act;
+	String title ="";
+	@FindBy(linkText = "Data Structures")
+	private WebElement DataStrIntroGetStarted;
+	
+	@FindBy(xpath="//a[@class='list-group-item']")
+	List<WebElement> GetStartedButtons;
+	
+	
+	@FindBy(xpath="//a[@href='/tryEditor']")
+	private WebElement tryHereButton;
+	
+	@FindBy(xpath="//form[@id='answer_form']/div/div/div/textarea")
+	private WebElement textAreaWorkSpace;
+	
+	@FindBy(xpath="//button[@type='button']")
+	private WebElement RunButton;
+	
+	@FindBy(id="output")
+	private WebElement output;
+	
+	String errorMessage;
+	
+	String message;
+	
+	public GraphPage_PageObject(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		act = new Actions(driver);
+	}
+	
+	public void loginPage()
+	{
+		driver.get("https://dsportalapp.herokuapp.com/login");
+	}
+	public void enterUserNamePassword(String userName ,String passWord)
+	{
+		driver.findElement(By.id("id_username")).sendKeys(userName);
+		driver.findElement(By.id("id_password")).sendKeys(passWord);
+		
+	}
+	public void clickLoginButton()
+	{
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		title = driver.getTitle();
+	}
+	public void homePage()
+	{
+		driver.get("https://dsportalapp.herokuapp.com/home");
+	}
+	public void selectGraphOption() throws InterruptedException
+	{
+		DataStrIntroGetStarted.click();
+		Thread.sleep(1000);
+		driver.findElement(By.linkText("Graph")).click();
+		title = driver.getTitle();
+	}
+	
+	public void getGraphPage()
+	{
+		driver.get("https://dsportalapp.herokuapp.com/graph/");
+		
+	}
+	public boolean click_GraphMenu_LinkForValid(String linkNameFromFeature)
+	{
+		
+		//List<WebElement> GetStartedButtons = driver.findElements(By.xpath("//a[@class='list-group-item']"));
+		boolean clicked =CommonUtils.webclick(GetStartedButtons, linkNameFromFeature);
+		return clicked;
+	}
+	
+	public void send_PythonCode_inGraph(boolean clickedMenu,String pythonCode)
+	{
+		
+		if(clickedMenu)
+		{
+			act.moveToElement(tryHereButton);
+			tryHereButton.click();
+			textAreaWorkSpace.sendKeys(pythonCode);
+		}
+	}
+	public void click_RunButton_ForValid_inGraph()
+	{
+		RunButton.click();
+	}
+	public String getTitle()
+	{
+		return title;
+	}
+	public String getMessage()
+	{
+		message = output.getText();
+		driver.navigate().to("https://dsportalapp.herokuapp.com/graph/");
+		return message;
+	}
+	
+	public String getErrorMessage()
+	{
+		errorMessage = driver.switchTo().alert().getText();
+		driver.switchTo().alert().accept();
+		driver.navigate().to("https://dsportalapp.herokuapp.com/home");
+		return errorMessage;
+	}
+	
+}
---
+++ DS_ALGO_PROJECT/src/test/resources/feature/09_GraphPageFeature.feature
@@ -0,0 +1,56 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+@tag
+Feature: To test the Graph Page
+
+Scenario: The user is logged into the HomePage
+Given The user is in login page to enter login details
+When The user is giving "Purnima1345@gmail.com" and "Kind@123"
+And the user presses Login Button
+Then The user is successfully logged into the homepage
+
+Scenario: The user navigates to Graph Data structure Page
+Given The user is in the HomePage
+When the user is selecting the Graph item from the drop down menu 
+Then the user navigates to the Graph Data Structure Page
+
+Scenario Outline: The user is able to navigate to Graph topic page and run code in the tryeditor
+Given The user is in the Graph Data Structure Page
+When the user clicks "<Graphoption>" in Grpah Data Structure page
+And the user enter valid "<PythonCode>" for Graph
+And the user clicks Run button for the Python code for Graph module
+Then the user see the graph module excepted "<output>"
+
+Examples:
+ | Graphoption                    |   PythonCode           |  output  |
+ | Graph                          | print('Hello, World!') |  Hello, World! |
+ | Graph Representations          | print('Hello, World!') |  Hello, World! |
+ 
+ Scenario Outline: The user is able to navigate to Graph topic page and runs code in the tryeditor page
+Given The user is present in the Graph Data Structure Page
+When the user click "<GraphoptionValue>" in Grpah Data Structure page
+And the user enters inValid "<PythonCode>" for Graph
+And the user clicks Run button in the tryEditor Page for the Python code for Graph module
+Then the user will get the graph module "<errormessage>"
+
+Examples:
+ | GraphoptionValue               |   PythonCode   |  errormessage  |
+ | Graph                          |    abcd        | NameError: name 'abcd' is not defined on line 1               |
+ | Graph Representations          |    abcd        | NameError: name 'abcd' is not defined on line 1               |
+ 
